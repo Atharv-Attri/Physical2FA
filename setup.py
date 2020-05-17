2c27dcbac68542efd9635bccb6e3da12fc6fc64b
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-05-17 12:08:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Physical2FA",  # Replace with your own username
-    version="0.0.b1",
+    version="0.0.b2",
     author="Atharv2",
     author_email="atharv260107@gmail.com",
     description="Alpha version",
