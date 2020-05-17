acdc5fd45ebdf983ad954e4e962b69b9303d0f39
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-05-16 20:50:33 -0700

    alpha

--- Physical2FA/encrypt.py
+++ Physical2FA/encrypt.py
@@ -40,46 +40,135 @@ def encrypt(filename, key):
 
 for i in files:
     encrypt(i, pubkey)
-while 1 < 100:
-    for i in directory:
-        os.chdir(i)
+
+for i in directory:
+    os.chdir(i)
+    array = os.listdir()
+    files = []
+    directory = []
+
+    print(array)
+    for i in array:
+        if os.path.isfile(i):
+            files.append(i)
+            if i == "encrypt.py":
+                array.remove("encrypt.py")
+        else:
+            print(directory.append(i))
+    for f in directory:
+        if f.startswith('.'):
+            directory.remove(f)
+            print("one file removed")
+    print(files)
+    print(directory)
+    for j in files:
+        encrypt(j, pubkey)
+    for j in directory:
+        os.chdir(j)
         array = os.listdir()
         files = []
         directory = []
-    
+
         print(array)
-        for i in array:
-            if os.path.isfile(i):
-                files.append(i)
-                if i == "encrypt.py":
+        for j in array:
+            if os.path.isfile(j):
+                files.append(j)
+                if j == "encrypt.py":
                     array.remove("encrypt.py")
             else:
                 print(directory.append(i))
-        for f in directory:
-            if f.startswith('.'):
-                directory.remove(f)
+        for j in directory:
+            if j.startswith('.'):
+                directory.remove(j)
                 print("one file removed")
         print(files)
         print(directory)
-        for i in files:
-            encrypt(i, pubkey)
-        
-        for i in directory:
-            os.chdir(i)
+            #NEXT
+        for k in files:
+            encrypt(k, pubkey)
+        for k in directory:
+            os.chdir(k)
             array = os.listdir()
             files = []
             directory = []
-        
+
             print(array)
-            for i in range(0, len(array)):
-                if os.path.isfile(array[i]):
-                    files.append(array[i])
+            for k in array:
+                if os.path.isfile(k):
+                    files.append(k)
+                    if k == "encrypt.py":
+                        array.remove("encrypt.py")
                 else:
-                    print(directory.append(array[i]))
-            for f in directory:
-                if f.startswith('.'):
-                    directory.remove(f)
+                    print(directory.append(k))
+            for k in directory:
+                if k.startswith('.'):
+                    directory.remove(k)
                     print("one file removed")
             print(files)
             print(directory)
-        
+        #NEXT
+            for l in directory:
+                os.chdir(l)
+                array = os.listdir()
+                files = []
+                directory = []
+
+                print(array)
+                for l in array:
+                    if os.path.isfile(l):
+                        files.append(l)
+                        if l == "encrypt.py":
+                            array.remove("encrypt.py")
+                    else:
+                        print(directory.append(l))
+                for f in directory:
+                    if f.startswith('.'):
+                        directory.remove(f)
+                        print("one file removed")
+                print(files)
+                print(directory)
+                for m in files:
+                    encrypt(m, pubkey)
+                for m in directory:
+                    os.chdir(m)
+                    array = os.listdir()
+                    files = []
+                    directory = []
+
+                    print(array)
+                    for m in array:
+                        if os.path.isfile(m):
+                            files.append(m)
+                            if m == "encrypt.py":
+                                array.remove("encrypt.py")
+                        else:
+                            print(directory.append(i))
+                    for f in directory:
+                        if f.startswith('.'):
+                            directory.remove(f)
+                            print("one file removed")
+                    print(files)
+                    print(directory)
+                    for n in files:
+                        encrypt(n, pubkey)
+                    for n in directory:
+                        os.chdir(n)
+                        array = os.listdir()
+                        files = []
+                        directory = []
+                    
+                        print(array)
+                        for i in array:
+                            if os.path.isfile(i):
+                                files.append(i)
+                                if i == "encrypt.py":
+                                    array.remove("encrypt.py")
+                            else:
+                                print(directory.append(i))
+                        for f in directory:
+                            if f.startswith('.'):
+                                directory.remove(f)
+                                print("one file removed")
+                        print(files)
+                        print(directory)
+                    
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Physical2FA",  # Replace with your own username
-    version="0.0.a4",
+    version="0.0.a6",
     author="Atharv2",
     author_email="atharv260107@gmail.com",
     description="Alpha version",
