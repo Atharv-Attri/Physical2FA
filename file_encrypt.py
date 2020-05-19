a8def779ac144fd699be1f951837889ec2da1a47
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-05-18 20:50:42 -0700

    Add files via upload

---
+++ file_encrypt.py
@@ -0,0 +1,41 @@
+from cryptography.fernet import Fernet
+
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key(name = 'key.key'):
+    return open(name, "rb").read()
+
+
+def encrypt(filename, key):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        # read all file data
+        file_data = file.read()
+    # encrypt data
+    encrypted_data = f.encrypt(file_data)
+    # write the encrypted file
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+
+
+def decrypt(filename, key):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        # read the encrypted data
+        encrypted_data = file.read()
+    # decrypt data
+    decrypted_data = f.decrypt(encrypted_data)
+    # write the original file
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+
+key = load_key("D:\key.key")
+filename = input("what file do you want to encrypt/decrypt? Please include the file extension: ")
+if input("do you want to encrypt or decrpyt(e/d): ") == 'e':
+    encrypt(filename, key)
+else: decrypt(filename, key)
